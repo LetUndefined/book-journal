eed83a2847cce6d09f8ae153b9527e3fa20c9045
Sandro <sandroodangelo@hotmail.com>
2026-01-22 23:29:24 +0100

    force

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = "https://bgqqexyhocbtiqdnrixq.supabase.co"
+const supabaseAnonKey = "sb_publishable_oaBZuO6-34zKp4w9wQP8xA_MFiykLwf"
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
